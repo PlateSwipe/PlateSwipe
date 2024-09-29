49f27116dd79ff829578d3a31c74084b1628c531
Ylanv <ylan.vifian@epfl.ch>
2024-09-29 22:36:53 +0200

    feat(build): add SonarCloud plugin
    Added the SonarCloud plugin to the build.gradle.kts file to enable
    automatic code analysis.This integration will ensure that code quality
    checks are performed on every build and PR.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,12 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+     id("org.sonarqube") version "4.4.1.3373"
+}
+sonar {
+  properties {
+    property("sonar.projectKey", "PlateSwipe_PlateSwipe")
+    property("sonar.organization", "plateswipe")
+    property("sonar.host.url", "https://sonarcloud.io")
+  }
 }
