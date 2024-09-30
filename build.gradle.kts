6d87e57b23b50737d48fb8c8f2ba5a10d5768f2a
Erik Hubner <erik.hubner@proton.me>
2024-09-30 18:35:32 +0200

    chore(ci-setup): add CI dependencies and google services features
    - Added google-services plugin to project-level and app-level build.gradle
    - Configured Firebase Firestore and Google Play Services dependencies
    - Updated CI pipeline to include Firestore integration
    - Ensured google-services.json is decoded in the CI environment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.gms) apply false
+    alias(libs.plugins.ktfmt) apply false
     id("org.sonarqube") version "4.4.1.3373"
 }
 sonar {
