eb86ef18b7cd63c6b2afbcad5d7449931104f11f
RagdollGoDown <zenooby2002@gmail.com>
2024-10-07 15:23:12 +0200

    change: IngredientRepository interface to support barcode get
    Added get function that uses ingredients barcode

--- app/src/main/java/com/android/sample/model/ingredient/IngredientRepository.kt
+++ app/src/main/java/com/android/sample/model/ingredient/IngredientRepository.kt
@@ -1,5 +1,12 @@
 package com.android.sample.model.ingredient
 
 interface IngredientRepository {
-    fun searchIngredients(name: String, onSuccess: (List<Ingredient>) -> Unit, onFailure: (Exception) -> Unit)
+  fun get(barCode: Long, onSuccess: (Ingredient) -> Unit, onFailure: (Exception) -> Unit)
+
+  fun search(
+      name: String,
+      onSuccess: (List<Ingredient>) -> Unit,
+      onFailure: (Exception) -> Unit,
+      count: Int = 20
+  )
 }
