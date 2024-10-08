ca3cade45d2c186c645b25c0eb19cd0fb89e6036
RagdollGoDown <zenooby2002@gmail.com>
2024-10-08 15:49:02 +0200

    change: using ingredient repo now returns null or empty when nothing is found
    Because we are searching the ingredient database without guarantee of finding it, it makes more sense that not finding a product is still a success and a failure is for when we get an exception.

--- app/src/main/java/com/android/sample/model/ingredient/IngredientRepository.kt
+++ app/src/main/java/com/android/sample/model/ingredient/IngredientRepository.kt
@@ -6,19 +6,19 @@ interface IngredientRepository {
    * Get an ingredient by its barcode.
    *
    * @param barCode The barcode of the ingredient. Can be any format of barcode (ex:EAN,UPC).
-   * @param onSuccess The callback to be called when the ingredient is found.
-   * @param onFailure The callback to be called when the ingredient is not found or an error occurs
-   *   when retrieving it.
+   * @param onSuccess The callback to be called when the ingredient is found. It's null if it isn't
+   *   found.
+   * @param onFailure The callback to be called when an error occurs during the get operation. when
+   *   retrieving it.
    */
-  fun get(barCode: Long, onSuccess: (Ingredient) -> Unit, onFailure: (Exception) -> Unit)
+  fun get(barCode: Long, onSuccess: (Ingredient?) -> Unit, onFailure: (Exception) -> Unit)
 
   /**
    * Search for ingredients by name.
    *
    * @param name The name of the ingredient to search for.
-   * @param onSuccess The callback to be called when the ingredients are found.
-   * @param onFailure The callback to be called when the ingredients are not found or an error
-   *   occurs when retrieving them.
+   * @param onSuccess The callback to be called with the resulting ingredients.
+   * @param onFailure The callback to be called with an exception if the search fails.
    * @param count The maximum number of ingredients to return.
    */
   fun search(
