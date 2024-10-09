18a228040162fd3ba54c05b0642a312928810c0a
Adrien Bader <Adrien Bader>
2024-10-09 21:27:24 +0200

    chore: update project dependencies
    Update Depedencies needed by the composable of the main page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -130,11 +130,16 @@ fun DependencyHandlerScope.globalTestImplementation(dep: Any) {
 }
 
 dependencies {
+    // Core
+    implementation(libs.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(platform(libs.compose.bom))
+    implementation(libs.androidx.espresso.intents)
     testImplementation(libs.junit)
     globalTestImplementation(libs.androidx.junit)
     globalTestImplementation(libs.androidx.espresso.core)
@@ -153,6 +158,20 @@ dependencies {
     globalTestImplementation(libs.compose.test.junit)
     debugImplementation(libs.compose.test.manifest)
 
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.material3)
+    implementation(platform(libs.androidx.compose.bom))
+    testImplementation(libs.test.core.ktx)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Navigation
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     globalTestImplementation(libs.kaspresso)
     globalTestImplementation(libs.kaspresso.compose)
 
@@ -186,6 +205,19 @@ dependencies {
     androidTestImplementation(libs.kaspresso.allure.support)
     androidTestImplementation(libs.kaspresso.compose.support)
     testImplementation(libs.kotlinx.coroutines.test)
+
+    // Code bar scanning
+    implementation(libs.mlkit.barcode.scanning)
+
+    // CameraX dependencies
+    implementation(libs.camerax.camera2)
+    implementation(libs.camerax.lifecycle)
+    implementation(libs.camerax.view)
+    implementation(libs.camerax.core)
+    implementation(libs.camerax.video)
+    implementation(libs.camerax.extensions)
+    implementation(libs.guava)
+    implementation(libs.accompanist.permissions)
 }
 
 tasks.withType<Test> {
