3f662faf408e8f6be27faec5cf4d7f18e7c4a83d
Erik Hübner <erik.hubner@protonmail.ch>
2024-10-09 16:43:37 +0200

    refractor : put the rememberPermission in the cameraScreen for testing purposes
    - Put the rememberCameraPermission from the CameraPermission file to the cameraScreen to simplify mocking it.
    
    - Rename the requestCameraPermission with uppercase at beginning.
    
    - Change the argument onPermissionDenied from
    () -> Unit to  @Composable () -> Unit.

--- app/src/main/java/com/android/sample/feature/camera/CameraPermission.kt
+++ app/src/main/java/com/android/sample/feature/camera/CameraPermission.kt
@@ -1,28 +1,26 @@
 package com.android.sample.feature.camera
 
 import androidx.compose.runtime.Composable
-import com.google.accompanist.permissions.rememberPermissionState
-import android.Manifest
 import androidx.compose.runtime.LaunchedEffect
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.PermissionState
 import com.google.accompanist.permissions.isGranted
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun requestCameraPermission(
+fun RequestCameraPermission(
+    cameraPermissionState: PermissionState,
     onPermissionGranted: @Composable () -> Unit,
-    onPermissionDenied: () -> Unit
+    onPermissionDenied: @Composable () -> Unit
 ) {
-    val cameraPermissionState = rememberPermissionState(Manifest.permission.CAMERA)
-
+  if (cameraPermissionState.status.isGranted) {
+    onPermissionGranted()
+  } else {
+    LaunchedEffect(Unit) { cameraPermissionState.launchPermissionRequest() }
     if (cameraPermissionState.status.isGranted) {
-        onPermissionGranted()
+      onPermissionGranted()
     } else {
-        LaunchedEffect(Unit) { cameraPermissionState.launchPermissionRequest() }
-        if (cameraPermissionState.status.isGranted) {
-            onPermissionGranted()
-        } else {
-            onPermissionDenied()
-        }
+      onPermissionDenied()
     }
+  }
 }
--- app/src/main/java/com/android/sample/ui/camera/cameraScreen.kt
+++ app/src/main/java/com/android/sample/ui/camera/cameraScreen.kt
@@ -1,42 +1,24 @@
 package com.android.sample.ui.camera
 
-import android.util.Log
+import android.Manifest
 import android.widget.Toast
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.core.content.ContextCompat
 import com.android.sample.feature.camera.CameraView
-import com.android.sample.feature.camera.requestCameraPermission
+import com.android.sample.feature.camera.RequestCameraPermission
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
-import java.io.File
-/**
- * Composable that displays the camera screen.
- */
+
+/** Composable that displays the camera screen. */
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 fun CameraScreen() {
-    val context = LocalContext.current
-    requestCameraPermission(
-        onPermissionGranted = {
-            CameraView()
-        },
-        onPermissionDenied = {
-            Toast.makeText(context, "Camera permission denied", Toast.LENGTH_SHORT).show()
-        }
-    )
+  val context = LocalContext.current
+  val cameraPermissionState = rememberPermissionState(Manifest.permission.CAMERA)
+  RequestCameraPermission(
+      cameraPermissionState = cameraPermissionState,
+      onPermissionGranted = { CameraView() },
+      onPermissionDenied = {
+        Toast.makeText(context, "Camera permission denied", Toast.LENGTH_SHORT).show()
+      })
 }
