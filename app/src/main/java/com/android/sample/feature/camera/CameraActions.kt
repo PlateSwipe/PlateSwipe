72bb9ae991ae57193cc807597f90a906eaa8496e
Ylanv <ylan.vifian@epfl.ch>
2024-10-09 16:17:52 +0200

    feature: save the photo as a MediaStore

--- app/src/main/java/com/android/sample/feature/camera/CameraActions.kt
+++ app/src/main/java/com/android/sample/feature/camera/CameraActions.kt
@@ -1,7 +1,10 @@
 package com.android.sample.feature.camera
 
+import android.content.ContentValues
 import android.content.Context
+import android.provider.MediaStore
 import android.util.Log
+import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
@@ -10,8 +13,26 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import com.android.sample.R
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
+import android.os.Build
 
+private const val LOG_TAG = "CAMERA_ACTIONS"
+private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+/**
+ * @author https://developer.android.com/codelabs/camerax-getting-started
+ * @param lifecycleOwner
+ * @param context
+ * @param imageCapture
+ * @param previewView
+ *
+ * @return Unit
+ * @throws Exception
+ *
+ * Function to start camera
+ */
 fun startCamera(lifecycleOwner: LifecycleOwner,context: Context,imageCapture: ImageCapture, previewView: PreviewView) {
     val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
     cameraProviderFuture.addListener({
@@ -28,27 +49,55 @@ fun startCamera(lifecycleOwner: LifecycleOwner,context: Context,imageCapture: Im
                 imageCapture
             )
         }catch(exc: Exception){
-            // Handle any errors
-            Log.e("CameraX", "Use case binding failed", exc)
+            Log.e(LOG_TAG, context.getString(R.string.use_case_binding_failed), exc)
         }
 
     }, ContextCompat.getMainExecutor(context))
 }
 
-fun takePhoto(context: Context, imageCapture: ImageCapture, onPhotoTaken: (File) -> Unit) {
-    val photoFile = File(context.filesDir, "${System.currentTimeMillis()}.jpg")
-    val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+/**
+ * @author https://developer.android.com/codelabs/camerax-getting-started
+ * @param context
+ * @param imageCapture
+ * @param onPhotoSaved
+ *
+ * @return Unit
+ * @throws ImageCaptureException
+ *
+ * Function to take photo
+ */
+fun takePhoto(context: Context, imageCapture: ImageCapture, onPhotoSaved: (ImageCapture.OutputFileResults) -> Unit) {
 
+    // Create time stamped name and MediaStore entry.
+    val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+        .format(System.currentTimeMillis())
+    val contentValues = ContentValues().apply {
+        put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+        put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+        }
+    }
+
+    // Create output options object which contains file + metadata
+    val outputOptions = ImageCapture.OutputFileOptions
+        .Builder(context.contentResolver,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues)
+        .build()
+
+    // Set up image capture listener, which is triggered after photo has
+    // been taken
     imageCapture.takePicture(
         outputOptions,
         ContextCompat.getMainExecutor(context),
         object : ImageCapture.OnImageSavedCallback {
-            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                onPhotoTaken(photoFile)
+            override fun onError(exc: ImageCaptureException) {
+                Log.e(LOG_TAG, "Photo capture failed: ${exc.message}", exc)
             }
 
-            override fun onError(exception: ImageCaptureException) {
-                // Handle error
+            override fun onImageSaved(output: ImageCapture.OutputFileResults){
+                onPhotoSaved(output)
             }
         }
     )
