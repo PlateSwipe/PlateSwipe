d128bfae767ad810b692317b2283f454ab1f9e83
CapraSnitzel <ricy_alex@yahoo.com>
2024-10-11 12:58:49 +0200

    docs: add user data class documentation
    Added the documentation of the user data class which describes each attribute required in order to create this class

--- app/src/main/java/com/android/sample/model/user/User.kt
+++ app/src/main/java/com/android/sample/model/user/User.kt
@@ -1,5 +1,14 @@
 package com.android.sample.model.user
 
+/**
+ *
+ * @param uid unique id of the user
+ * @param userName user name that will be displayed in the account page
+ * @param profilePictureUrl profile picture that will be displayed in the account page
+ * @param fridge list of ingredients
+ * @param savedRecipes list of saved/liked recipes
+ * @param createdRecipes list of created recipes
+ */
 data class User(
     val uid: String,
     val userName: String,
