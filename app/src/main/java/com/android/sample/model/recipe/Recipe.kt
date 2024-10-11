657022bc031e54a78a807c371877aed9e9cccf0a
2Fick <youcef.amar@epfl.ch>
2024-10-11 12:59:53 +0200

    refactor(recipe): update Recipe model structure
    Made strCategory and strArea optional to enhance flexibility. Renamed strMealThumb to strMealThumbUrl for clarity. Changed ingredients to List<Long> for ingredient IDs to align with requirements.

--- app/src/main/java/com/android/sample/model/recipe/Recipe.kt
+++ app/src/main/java/com/android/sample/model/recipe/Recipe.kt
@@ -3,10 +3,10 @@ package com.android.sample.model.recipe
 data class Recipe(
     val idMeal: Long,
     val strMeal: String,
-    val strCategory: String,
-    val strArea: String,
+    val strCategory: String?,
+    val strArea: String?,
     val strInstructions: String,
-    val strMealThumb: String,
-    val ingredients: List<String>, // List of ingredients
-    val measurements: List<String> // Corresponding measurements
+    val strMealThumbUrl: String,
+    val ingredients: List<Long>,
+    val measurements: List<String>
 )
