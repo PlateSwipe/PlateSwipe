c3223e2276e908748606a54df6e23e6d5a9cfc4a
Adrien Bader <Adrien Bader>
2024-10-13 15:13:11 +0200

    refactor: Update Intents.init/release and remove unused variable
    - Modified `MainPageTest.kt` to update Intents initialization and release
    - Updated `SwipePage.kt` by deleting an unused variable

--- app/src/androidTest/java/com/android/sample/screen/MainPageTest.kt
+++ app/src/androidTest/java/com/android/sample/screen/MainPageTest.kt
@@ -6,11 +6,13 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTouchInput
 import androidx.compose.ui.test.swipeLeft
+import androidx.test.espresso.intent.Intents
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Route
 import com.android.sample.ui.swipePage.SwipePage
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -20,46 +22,53 @@ import org.mockito.Mockito.`when`
 
 @RunWith(AndroidJUnit4::class)
 class MainPageTest : TestCase() {
-  private lateinit var navigationActions: NavigationActions
+    private lateinit var navigationActions: NavigationActions
 
-  @get:Rule val composeTestRule = createComposeRule()
+    @get:Rule
+    val composeTestRule = createComposeRule()
 
-  @Before
-  fun setUp() {
-    navigationActions = mock(NavigationActions::class.java)
-    `when`(navigationActions.currentRoute()).thenReturn(Route.MAIN)
+    @Before
+    fun setUp() {
+        Intents.init()
+        navigationActions = mock(NavigationActions::class.java)
+        `when`(navigationActions.currentRoute()).thenReturn(Route.MAIN)
 
-    composeTestRule.setContent {
-      SwipePage(navigationActions) // Set up the SignInScreen directly
+        composeTestRule.setContent {
+            SwipePage(navigationActions) // Set up the SignInScreen directly
+        }
     }
-  }
 
-  @Test
-  fun recipeAndDescriptionAreCorrectlyDisplayed() {
-    composeTestRule.onNodeWithTag("recipeDescription").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("recipeImage").assertIsDisplayed()
-    composeTestRule.onNodeWithTag("swipeUIDescription").assertIsDisplayed()
-  }
-
-  @Test
-  fun testBiggerDescription() {
-    // Make sure the screen is displayed
-    composeTestRule.onNodeWithTag("displayDescription").assertIsDisplayed()
-
-    // Simulate a drag event
-    composeTestRule.onNodeWithTag("displayDescription").performClick()
-    composeTestRule.onNodeWithTag("RecipeEntireDescription").assertIsDisplayed()
-  }
-
-  @Test
-  fun testDraggingEvent() {
-    // Make sure the screen is displayed
-
-    composeTestRule.onNodeWithTag("draggableItem").assertIsDisplayed()
-
-    // Simulate a drag event
-    composeTestRule.onNodeWithTag("draggableItem").performTouchInput {
-      swipeLeft(durationMillis = 5000)
+    @After
+    fun tearDown() {
+        Intents.release()
     }
-  }
+
+    @Test
+    fun recipeAndDescriptionAreCorrectlyDisplayed() {
+        composeTestRule.onNodeWithTag("recipeDescription").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("recipeImage").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("swipeUIDescription").assertIsDisplayed()
+    }
+
+    @Test
+    fun testBiggerDescription() {
+        // Make sure the screen is displayed
+        composeTestRule.onNodeWithTag("displayDescription").assertIsDisplayed()
+
+        // Simulate a drag event
+        composeTestRule.onNodeWithTag("displayDescription").performClick()
+        composeTestRule.onNodeWithTag("RecipeEntireDescription").assertIsDisplayed()
+    }
+
+    @Test
+    fun testDraggingEvent() {
+        // Make sure the screen is displayed
+
+        composeTestRule.onNodeWithTag("draggableItem").assertIsDisplayed()
+
+        // Simulate a drag event
+        composeTestRule.onNodeWithTag("draggableItem").performTouchInput {
+            swipeLeft(durationMillis = 5000)
+        }
+    }
 }
--- app/src/main/java/com/android/sample/ui/swipePage/SwipePage.kt
+++ app/src/main/java/com/android/sample/ui/swipePage/SwipePage.kt
@@ -59,24 +59,27 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SwipePage(navigationActions: NavigationActions) {
-  val selectedItem = navigationActions.currentRoute()
+    val selectedItem = navigationActions.currentRoute()
 
-  Scaffold(
-      topBar = {
-        TopAppBar(
-            title = { Text("PlateSwipe") },
-            colors =
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("PlateSwipe") },
+                colors =
                 TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primary))
-      },
-      bottomBar = {
-        BottomNavigationMenu(
-            onTabSelect = { tab -> navigationActions.navigateTo(tab) },
-            tabList = LIST_TOP_LEVEL_DESTINATIONS,
-            selectedItem = selectedItem)
-      }) { paddingValues ->
+                    containerColor = MaterialTheme.colorScheme.primary
+                )
+            )
+        },
+        bottomBar = {
+            BottomNavigationMenu(
+                onTabSelect = { tab -> navigationActions.navigateTo(tab) },
+                tabList = LIST_TOP_LEVEL_DESTINATIONS,
+                selectedItem = selectedItem
+            )
+        }) { paddingValues ->
         ImageSwipeGallery(paddingValues)
-      }
+    }
 }
 
 /**
@@ -86,72 +89,82 @@ fun SwipePage(navigationActions: NavigationActions) {
  */
 @Composable
 fun ImageSwipeGallery(paddingValues: PaddingValues) {
-  val height = LocalConfiguration.current.screenHeightDp.dp * 1 / 2
-  val width = height * 3 / 4
-  var isDescriptionVisible by remember {
-    mutableStateOf(false)
-  } // Tracks if the text should be visible
+    val height = LocalConfiguration.current.screenHeightDp.dp * 1 / 2
+    val width = height * 3 / 4
+    var isDescriptionVisible by remember {
+        mutableStateOf(false)
+    } // Tracks if the text should be visible
 
-  var currentImageId by remember { mutableIntStateOf(R.drawable.burger) }
-  val offsetX = remember { Animatable(0f) } // For tracking drag offset
-  var isDragging by remember { mutableStateOf(false) } // Flag to track if currently dragging
-  val coroutineScope = rememberCoroutineScope() // Coroutine scope for animations
-  val context = LocalContext.current
-  // Get the screen width to manage swipe gestures
-  val screenWidth = LocalConfiguration.current.screenWidthDp.toFloat()
-  // Handle gesture end to check if we need to change images
-  val swipeThreshold = screenWidth / 4
+    var currentImageId by remember { mutableIntStateOf(R.drawable.burger) }
+    val offsetX = remember { Animatable(0f) } // For tracking drag offset
+    val coroutineScope = rememberCoroutineScope() // Coroutine scope for animations
+    val context = LocalContext.current
+    // Get the screen width to manage swipe gestures
+    val screenWidth = LocalConfiguration.current.screenWidthDp.toFloat()
+    // Handle gesture end to check if we need to change images
+    val swipeThreshold = screenWidth / 4
 
-  Column(
-      modifier =
-          Modifier.testTag("draggableItem")
-              .fillMaxSize()
-              .padding(paddingValues)
-              .padding(16.dp)
-              .pointerInput(Unit) {
+    Column(
+        modifier =
+        Modifier
+            .testTag("draggableItem")
+            .fillMaxSize()
+            .padding(paddingValues)
+            .padding(16.dp)
+            .pointerInput(Unit) {
                 detectHorizontalDragGestures(
                     onDragEnd = {
-                      if (offsetX.value > swipeThreshold) {
-                        // Right Swipe
-                        isDescriptionVisible = false
-                        currentImageId = setNextImage(currentImageId)
-                        // hard coded toast for now
-                        val toast =
-                            Toast.makeText(context, "Dislike", Toast.LENGTH_SHORT) // in Activity
-                        toast.show()
-                      } else if (offsetX.value < -swipeThreshold) {
-                        // Left Swipe
-                        isDescriptionVisible = false
-                        currentImageId = setNextImage(currentImageId)
-                        // hard coded toast for now
-                        val toast =
-                            Toast.makeText(context, "Like", Toast.LENGTH_SHORT) // in Activity
-                        toast.show()
-                      }
+                        if (offsetX.value > swipeThreshold) {
+                            // Right Swipe
+                            isDescriptionVisible = false
+                            currentImageId = setNextImage(currentImageId)
+                            // hard coded toast for now
+                            val toast =
+                                Toast.makeText(
+                                    context,
+                                    "Dislike",
+                                    Toast.LENGTH_SHORT
+                                )
+                            toast.show()
+                        } else if (offsetX.value < -swipeThreshold) {
+                            // Left Swipe
+                            isDescriptionVisible = false
+                            currentImageId = setNextImage(currentImageId)
+                            // hard coded toast for now
+                            val toast =
+                                Toast.makeText(context, "Like", Toast.LENGTH_SHORT) // in Activity
+                            toast.show()
+                        }
                     },
-                    onHorizontalDrag = { change, dragAmount ->
-                      coroutineScope.launch { offsetX.snapTo(offsetX.value + dragAmount) }
+                    onHorizontalDrag = { _, dragAmount ->
+                        coroutineScope.launch { offsetX.snapTo(offsetX.value + dragAmount) }
                     })
-              }) {
+            }) {
         Card(
             modifier =
-                Modifier.fillMaxWidth().padding(8.dp).graphicsLayer(translationX = offsetX.value),
+            Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+                .graphicsLayer(translationX = offsetX.value),
             shape = RoundedCornerShape(16.dp),
-            elevation = CardDefaults.cardElevation(4.dp)) {
-              Column(modifier = Modifier.background(color = MaterialTheme.colorScheme.onPrimary)) {
+            elevation = CardDefaults.cardElevation(4.dp)
+        ) {
+            Column(modifier = Modifier.background(color = MaterialTheme.colorScheme.onPrimary)) {
                 Image(
                     painter = painterResource(id = currentImageId),
                     contentDescription = "Recipe Image",
                     modifier =
-                        Modifier.testTag("recipeImage")
-                            .fillMaxWidth()
-                            .size(
-                                width = width,
-                                height = if (isDescriptionVisible) height * 1 / 2 else height),
+                    Modifier
+                        .testTag("recipeImage")
+                        .fillMaxWidth()
+                        .size(
+                            width = width,
+                            height = if (isDescriptionVisible) height * 1 / 2 else height
+                        ),
                     contentScale = ContentScale.FillWidth,
                 )
-              }
             }
+        }
         // Display Recipe Description
 
         Description(
@@ -166,19 +179,22 @@ fun ImageSwipeGallery(paddingValues: PaddingValues) {
             text = "Swipe to like or dislike the recipe.",
             style = MaterialTheme.typography.bodyMedium,
             color = MaterialTheme.colorScheme.onSecondary,
-            modifier = Modifier.testTag("swipeUIDescription").padding(16.dp))
+            modifier = Modifier
+                .testTag("swipeUIDescription")
+                .padding(16.dp)
+        )
 
         // Animate back to center if not swiped
         LaunchedEffect(offsetX.value) {
-          offsetX.animateTo(0f, animationSpec = tween(50)) // Animate back to center
+            offsetX.animateTo(0f, animationSpec = tween(50)) // Animate back to center
         }
-      }
+    }
 }
 
 /** Set the next image based on the current image. Hardcoded for now */
 private fun setNextImage(current: Int): Int {
-  if (current == R.drawable.burger) return R.drawable.salad
-  return R.drawable.burger
+    if (current == R.drawable.burger) return R.drawable.salad
+    return R.drawable.burger
 }
 
 /**
@@ -190,56 +206,62 @@ private fun setNextImage(current: Int): Int {
 @Composable
 private fun Description(isDescriptionVisible: Boolean, modifier: Modifier) {
 
-  Column(modifier = Modifier.testTag("recipeDescription").padding(16.dp)) {
+    Column(modifier = Modifier
+        .testTag("recipeDescription")
+        .padding(16.dp)) {
 
-    // Display Recipe Name
-    Text(
-        modifier = modifier,
-        text = "Pasta",
-        style = MaterialTheme.typography.bodyLarge,
-        color = MaterialTheme.colorScheme.onSecondary,
-    )
-    if (!isDescriptionVisible) {
-      // Display "..." if the description is not visible
-      Text(
-          modifier = modifier.testTag("displayDescription"),
-          text = "...",
-          style = MaterialTheme.typography.bodyLarge,
-          color = MaterialTheme.colorScheme.onSecondary,
-      )
-    }
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-      // Display with animation Recipe Description
-      AnimatedVisibility(
-          visible = isDescriptionVisible, enter = fadeIn(animationSpec = tween(500))) {
-            Text(
-                modifier = Modifier.testTag("RecipeEntireDescription"),
-                text = getRecipeDescription(),
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.onSecondary,
-            )
-          }
-
-      Spacer(modifier = Modifier.padding(5.dp))
-
-      // Display Recipe Timing
-      Row(modifier = Modifier.padding(16.dp)) {
-        Icon(painter = painterResource(R.drawable.time_line), contentDescription = "recipes timing")
+        // Display Recipe Name
         Text(
-            text = "30min",
+            modifier = modifier,
+            text = "Pasta",
             style = MaterialTheme.typography.bodyLarge,
-            fontSize = 22.sp,
             color = MaterialTheme.colorScheme.onSecondary,
         )
-      }
+        if (!isDescriptionVisible) {
+            // Display "..." if the description is not visible
+            Text(
+                modifier = modifier.testTag("displayDescription"),
+                text = "...",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSecondary,
+            )
+        }
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+            // Display with animation Recipe Description
+            AnimatedVisibility(
+                visible = isDescriptionVisible, enter = fadeIn(animationSpec = tween(500))
+            ) {
+                Text(
+                    modifier = Modifier.testTag("RecipeEntireDescription"),
+                    text = getRecipeDescription(),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSecondary,
+                )
+            }
+
+            Spacer(modifier = Modifier.padding(5.dp))
+
+            // Display Recipe Timing
+            Row(modifier = Modifier.padding(16.dp)) {
+                Icon(
+                    painter = painterResource(R.drawable.time_line),
+                    contentDescription = "recipes timing"
+                )
+                Text(
+                    text = "30min",
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontSize = 22.sp,
+                    color = MaterialTheme.colorScheme.onSecondary,
+                )
+            }
+        }
     }
-  }
 }
 
 /** @return The recipe description hard coded for now */
 @Composable
 private fun getRecipeDescription(): String {
-  return "Delicious pasta with tomato sauce. In a separate pan, prepare the sauce with garlic, onions, and fresh tomatoes. Combine the cooked pasta with the sauce, and serve hot. Enjoy your meal! Ingredients: Basil, Tomato, Pasta" +
-      "Delicious pasta with tomato sauce. In a separate pan, prepare the sauce with garlic, onions, and fresh tomatoes. Combine the cooked pasta with the sauce, and serve hot. Enjoy your meal! Ingredients: Basil, Tomato, Pasta" +
-      "Delicious pasta with tomato sauce. In a separate pan, prepare the sauce with garlic, onions, and fresh tomatoes. Combine the cooked pasta with the sauce, and serve hot. Enjoy your meal! Ingredients: Basil, Tomato, Pasta"
+    return "Delicious pasta with tomato sauce. In a separate pan, prepare the sauce with garlic, onions, and fresh tomatoes. Combine the cooked pasta with the sauce, and serve hot. Enjoy your meal! Ingredients: Basil, Tomato, Pasta" +
+            "Delicious pasta with tomato sauce. In a separate pan, prepare the sauce with garlic, onions, and fresh tomatoes. Combine the cooked pasta with the sauce, and serve hot. Enjoy your meal! Ingredients: Basil, Tomato, Pasta" +
+            "Delicious pasta with tomato sauce. In a separate pan, prepare the sauce with garlic, onions, and fresh tomatoes. Combine the cooked pasta with the sauce, and serve hot. Enjoy your meal! Ingredients: Basil, Tomato, Pasta"
 }
