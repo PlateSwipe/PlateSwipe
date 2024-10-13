de55d44235d22ea3722601b52cbac2da8f08dd6d
Adrien Bader <Adrien Bader>
2024-10-13 12:45:19 +0200

    chore: Remove unused test and apply ktfmt formatting to MainActivity
    - Deleted unused `MainScreenTest.kt`
    - Applied ktfmt formatting to `MainActivity.kt` for code consistency

--- app/src/main/java/com/android/sample/MainActivity.kt
+++ app/src/main/java/com/android/sample/MainActivity.kt
@@ -29,67 +29,64 @@ import com.android.sample.ui.testScreens.SearchScreen
 import com.android.sample.ui.theme.SampleAppTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            SampleAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .semantics { testTag = C.Tag.main_screen_container },
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    PlateSwipeApp()
-                }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      SampleAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize().semantics { testTag = C.Tag.main_screen_container },
+            color = MaterialTheme.colorScheme.background) {
+              PlateSwipeApp()
             }
-        }
+      }
     }
+  }
 }
 
 @Composable
 fun PlateSwipeApp() {
-    val navController = rememberNavController()
-    val navigationActions = NavigationActions(navController)
+  val navController = rememberNavController()
+  val navigationActions = NavigationActions(navController)
 
-    NavHost(navController = navController, startDestination = Route.AUTH) {
-        navigation(
-            startDestination = Screen.AUTH,
-            route = Route.AUTH,
-        ) {
-            composable(Screen.AUTH) { AuthScreen() }
-        }
-        navigation(
-            startDestination = Screen.MAIN,
-            route = Route.MAIN,
-        ) {
-            composable(Screen.MAIN) { SwipePage(navigationActions) }
-        }
-        navigation(
-            startDestination = Screen.FRIDGE,
-            route = Route.FRIDGE,
-        ) {
-            composable(Screen.FRIDGE) { FridgeScreen(navigationActions) }
-            composable(Screen.INGREDIENT) { IngredientScreen() }
-        }
-        navigation(
-            startDestination = Screen.SEARCH,
-            route = Route.SEARCH,
-        ) {
-            composable(Screen.SEARCH) { SearchScreen(navigationActions) }
-            composable(Screen.RECIPE) { RecipeScreen() }
-        }
-        navigation(
-            startDestination = Screen.ADD_RECIPE,
-            route = Route.ADD_RECIPE,
-        ) {
-            composable(Screen.ADD_RECIPE) { AddRecipeScreen(navigationActions) }
-        }
-        navigation(
-            startDestination = Screen.ACCOUNT,
-            route = Route.ACCOUNT,
-        ) {
-            composable(Screen.ACCOUNT) { AccountScreen(navigationActions) }
-        }
+  NavHost(navController = navController, startDestination = Route.AUTH) {
+    navigation(
+        startDestination = Screen.AUTH,
+        route = Route.AUTH,
+    ) {
+      composable(Screen.AUTH) { AuthScreen() }
     }
+    navigation(
+        startDestination = Screen.MAIN,
+        route = Route.MAIN,
+    ) {
+      composable(Screen.MAIN) { SwipePage(navigationActions) }
+    }
+    navigation(
+        startDestination = Screen.FRIDGE,
+        route = Route.FRIDGE,
+    ) {
+      composable(Screen.FRIDGE) { FridgeScreen(navigationActions) }
+      composable(Screen.INGREDIENT) { IngredientScreen() }
+    }
+    navigation(
+        startDestination = Screen.SEARCH,
+        route = Route.SEARCH,
+    ) {
+      composable(Screen.SEARCH) { SearchScreen(navigationActions) }
+      composable(Screen.RECIPE) { RecipeScreen() }
+    }
+    navigation(
+        startDestination = Screen.ADD_RECIPE,
+        route = Route.ADD_RECIPE,
+    ) {
+      composable(Screen.ADD_RECIPE) { AddRecipeScreen(navigationActions) }
+    }
+    navigation(
+        startDestination = Screen.ACCOUNT,
+        route = Route.ACCOUNT,
+    ) {
+      composable(Screen.ACCOUNT) { AccountScreen(navigationActions) }
+    }
+  }
 }
