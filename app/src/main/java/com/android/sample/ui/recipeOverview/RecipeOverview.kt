62274d8cd4d5337bb7ac831a95681a7273d9c88d
CapraSnitzel <ricy_alex@yahoo.com>
2024-10-17 22:47:44 +0200

    feat: add recipe overview
    Added the recipe overview screen where we can view a specific recipe.

---
+++ app/src/main/java/com/android/sample/ui/recipeOverview/RecipeOverview.kt
@@ -0,0 +1,295 @@
+package com.android.sample.ui.recipeOverview
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+import com.android.sample.model.recipe.RecipesViewModel
+import com.android.sample.ui.navigation.BottomNavigationMenu
+import com.android.sample.ui.navigation.LIST_TOP_LEVEL_DESTINATIONS
+import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.theme.goldenBronze
+import com.android.sample.ui.theme.starColor
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RecipeOverview(navigationActions: NavigationActions, recipesViewModel: RecipesViewModel) {
+  val selectedItem = navigationActions.currentRoute()
+  val height = LocalConfiguration.current.screenHeightDp.dp * 1 / 2
+  val width = height * 3 / 4
+  val currentRecipe by recipesViewModel.currentRecipe.collectAsState()
+  var ingredientsView by remember { mutableStateOf(false) }
+  var servingsCount by remember { mutableStateOf(0) }
+  val scrollState = rememberScrollState()
+
+  Scaffold(
+      contentColor = MaterialTheme.colorScheme.background,
+      topBar = {
+        CenterAlignedTopAppBar(
+            title = { Text("PlateSwipe") },
+            modifier = Modifier.testTag("topBar"),
+            navigationIcon = {
+              IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Return button")
+              }
+            })
+      },
+      bottomBar = {
+        BottomNavigationMenu(
+            onTabSelect = { tab -> navigationActions.navigateTo(tab) },
+            tabList = LIST_TOP_LEVEL_DESTINATIONS,
+            selectedItem = selectedItem)
+      }) { paddingValues ->
+        Column(
+            modifier =
+                Modifier.testTag("draggableItem")
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp)
+                    .verticalScroll(scrollState)) {
+              //
+              Card(
+                  modifier = Modifier.fillMaxWidth().padding(8.dp),
+                  shape = RoundedCornerShape(16.dp),
+                  elevation = CardDefaults.cardElevation(4.dp)) {
+                    Column(
+                        modifier =
+                            Modifier.background(color = MaterialTheme.colorScheme.onPrimary)) {
+                          Image(
+                              painter =
+                                  rememberAsyncImagePainter(model = currentRecipe?.strMealThumbUrl),
+                              contentDescription = "Recipe Image",
+                              modifier =
+                                  Modifier.fillMaxWidth()
+                                      .size(width = width, height = height)
+                                      .testTag("recipeImage"),
+                              contentScale = ContentScale.FillHeight,
+                          )
+                        }
+                  }
+              Column {
+                Row {
+                  currentRecipe?.let {
+                    Text(text = it.strMeal, fontSize = 20.sp, color = Color.Black)
+                  }
+                }
+                Spacer(modifier = Modifier.size(17.dp))
+                Row(horizontalArrangement = Arrangement.Start) {
+                  Icon(
+                      imageVector = Icons.Filled.Star,
+                      contentDescription = "Rating",
+                      tint = starColor,
+                      modifier = Modifier.width(12.dp).height(12.dp))
+                  Spacer(modifier = Modifier.size(8.dp))
+                  Row { Text(text = "Rating", fontSize = 12.sp, color = Color.Black) }
+                  Spacer(modifier = Modifier.size(8.dp))
+                  Row {
+                    currentRecipe?.strCategory?.let {
+                      Text(text = it, fontSize = 12.sp, color = Color.Black)
+                    }
+                  }
+                }
+              }
+              Spacer(modifier = Modifier.size(16.dp))
+              Row(
+                  modifier =
+                      Modifier.fillMaxWidth(0.85f)
+                          .padding(start = 55.dp)
+                          .background(
+                              MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(10)),
+                  horizontalArrangement = Arrangement.Center,
+              ) {
+                Column {
+                  Text("Prep time", fontSize = 12.sp, color = Color.Black)
+                  Spacer(modifier = Modifier.size(14.dp))
+                  Text("30 min", fontSize = 12.sp, color = Color.Black)
+                }
+                Spacer(modifier = Modifier.size(40.dp))
+                Column {
+                  Text("Cook time", fontSize = 12.sp, color = Color.Black)
+                  Spacer(modifier = Modifier.size(14.dp))
+                  Text("20 min", fontSize = 12.sp, color = Color.Black)
+                }
+                Spacer(modifier = Modifier.size(40.dp))
+                Column {
+                  Text("Total time", fontSize = 12.sp, color = Color.Black)
+                  Spacer(modifier = Modifier.size(14.dp))
+                  Text("50 min", fontSize = 12.sp, color = Color.Black)
+                }
+              }
+              Spacer(modifier = Modifier.size(14.dp))
+              Column(
+                  horizontalAlignment = Alignment.CenterHorizontally,
+                  modifier =
+                      Modifier.background(
+                              MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(5))
+                          .fillMaxSize()) {
+                    Row {
+                      Button(
+                          onClick = { ingredientsView = true },
+                          shape = RoundedCornerShape(0.dp),
+                          modifier = Modifier.width(150.dp),
+                          colors =
+                              ButtonColors(goldenBronze, Color.Black, goldenBronze, Color.Black),
+                          border = BorderStroke(2.dp, Color.Black)) {
+                            Text(
+                                text = "Ingredients",
+                                fontSize = 12.sp,
+                                textAlign = TextAlign.Center)
+                          }
+                      Button(
+                          onClick = { ingredientsView = false },
+                          shape = RoundedCornerShape(0.dp),
+                          modifier = Modifier.width(150.dp),
+                          colors =
+                              ButtonColors(goldenBronze, Color.Black, goldenBronze, Color.Black),
+                          border = BorderStroke(2.dp, Color.Black)) {
+                            Text(
+                                text = "Instructions",
+                                fontSize = 12.sp,
+                                textAlign = TextAlign.Center)
+                          }
+                    }
+                    Column {
+                      if (ingredientsView) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center) {
+                              Text(
+                                  "Servings",
+                                  textAlign = TextAlign.Left,
+                                  color = Color.Black,
+                                  fontSize = 12.sp,
+                                  modifier = Modifier.width(56.dp).height(18.dp))
+                              Spacer(modifier = Modifier.width(190.dp))
+                              Row(
+                                  modifier =
+                                      Modifier.background(
+                                              goldenBronze, shape = RoundedCornerShape(25))
+                                          .height(25.dp)
+                                          .width(53.dp),
+                                  horizontalArrangement = Arrangement.Center,
+                                  verticalAlignment = Alignment.CenterVertically) {
+                                    Button(
+                                        onClick = { if (servingsCount > 0) --servingsCount },
+                                        colors =
+                                            ButtonColors(
+                                                goldenBronze,
+                                                Color.Black,
+                                                goldenBronze,
+                                                Color.Black),
+                                        modifier = Modifier.size(20.dp),
+                                        contentPadding = PaddingValues()) {
+                                          Text("-", fontSize = 12.sp)
+                                        }
+                                    Text(
+                                        servingsCount.toString(),
+                                        fontSize = 12.sp,
+                                        color = Color.Black)
+                                    Button(
+                                        onClick = { ++servingsCount },
+                                        colors =
+                                            ButtonColors(
+                                                goldenBronze,
+                                                Color.Black,
+                                                goldenBronze,
+                                                Color.Black),
+                                        modifier = Modifier.size(20.dp),
+                                        contentPadding = PaddingValues()) {
+                                          Text("+", fontSize = 12.sp)
+                                        }
+                                  }
+                            }
+                        Spacer(modifier = Modifier.height(7.dp))
+                        Column {
+                          currentRecipe?.ingredientsAndMeasurements?.forEach {
+                              (ingredient, measurement) ->
+                            var ticked by remember { mutableStateOf(false) }
+                            Row(
+                                modifier = Modifier.padding(start = 56.dp),
+                                verticalAlignment = Alignment.CenterVertically) {
+                                  Checkbox(
+                                      checked = ticked,
+                                      onCheckedChange = { ticked = it },
+                                      modifier = Modifier.size(15.dp))
+                                  Spacer(modifier = Modifier.width(6.dp))
+                                  Text(
+                                      ingredient,
+                                      textAlign = TextAlign.Left,
+                                      fontSize = 12.sp,
+                                      color = Color.Black)
+                                  Spacer(modifier = Modifier.width(6.dp))
+                                  Text(
+                                      measurement,
+                                      textAlign = TextAlign.Left,
+                                      fontSize = 12.sp,
+                                      color = Color.Black)
+                                }
+                            Spacer(modifier = Modifier.height(12.dp))
+                          }
+                        }
+                      } else {
+                        Column(
+                            modifier =
+                                Modifier.padding(
+                                    start = 25.dp, end = 15.dp, top = 10.dp, bottom = 5.dp)) {
+                              currentRecipe?.let {
+                                Text(
+                                    text = it.strInstructions,
+                                    color = Color.Black,
+                                    fontSize = 16.sp)
+                              }
+                            }
+                      }
+                    }
+                  }
+            }
+      }
+}
