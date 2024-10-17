550a50dad1f5bf438a6e254a1e19dc414fb38535
Ylanv <ylan.vifian@epfl.ch>
2024-10-17 16:31:34 +0200

    feat(fridge): add FridgeScreen
    navigation and basic test

---
+++ app/src/androidTest/java/com/android/sample/fridge/FridgeScreenTest.kt
@@ -0,0 +1,38 @@
+package com.android.sample.fridge
+
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.android.sample.ui.fridge.FridgeScreen
+import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.navigation.Screen
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+
+class FridgeScreenTest {
+
+
+    private lateinit var navigationActions: NavigationActions
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun setUp() {
+        navigationActions = mock(NavigationActions::class.java)
+        `when`(navigationActions.currentRoute()).thenReturn(Screen.FRIDGE)
+    }
+
+    @Test
+    fun mainTextIsDisplayed() {
+        composeTestRule.setContent {
+            FridgeScreen(navigationActions = navigationActions)
+        }
+        composeTestRule.onNodeWithText("Fridge Screen").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Work in progress... Stay tuned!").assertIsDisplayed()
+    }
+}
---
+++ app/src/main/java/com/android/sample/ui/fridge/FridgeScreen.kt
@@ -0,0 +1,41 @@
+package com.android.sample.ui.fridge
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.android.sample.ui.navigation.BottomNavigationMenu
+import com.android.sample.ui.navigation.LIST_TOP_LEVEL_DESTINATIONS
+import com.android.sample.ui.navigation.NavigationActions
+
+
+@Composable
+fun FridgeScreen(
+    navigationActions: NavigationActions
+) {
+    Scaffold(
+        bottomBar = {
+            BottomNavigationMenu(
+                onTabSelect = { tab -> navigationActions.navigateTo(tab) },
+                tabList = LIST_TOP_LEVEL_DESTINATIONS,
+                selectedItem = navigationActions.currentRoute())
+        }) { paddingValues ->
+        Box(
+            modifier = Modifier.fillMaxSize().padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Fridge Screen", style = MaterialTheme.typography.titleLarge)
+                Text("Work in progress... Stay tuned!", style = MaterialTheme.typography.bodyMedium)
+            }
+        }
+    }
+}
