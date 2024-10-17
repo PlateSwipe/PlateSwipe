8e199e704353f098656ee4b1adeb2f33bdf6bc41
Ylanv <ylan.vifian@epfl.ch>
2024-10-17 16:33:24 +0200

    feat(recipe) : add Search & Create Recipe screen
    Two screen with basis UI,navigation and test

---
+++ app/src/androidTest/java/com/android/sample/recipe/CreateRecipeScreenTest.kt
@@ -0,0 +1,37 @@
+package com.android.sample.recipe
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.android.sample.ui.fridge.FridgeScreen
+import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.navigation.Screen
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+
+class CreateRecipeScreenTest {
+
+
+    private lateinit var navigationActions: NavigationActions
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Before
+    fun setUp() {
+        navigationActions = mock(NavigationActions::class.java)
+        `when`(navigationActions.currentRoute()).thenReturn(Screen.CREATE_RECIPE)
+    }
+
+    @Test
+    fun mainTextIsDisplayed() {
+        composeTestRule.setContent {
+            FridgeScreen(navigationActions = navigationActions)
+        }
+        composeTestRule.onNodeWithText("Create Recipe Screen").assertIsDisplayed()
+        composeTestRule.onNodeWithText("Work in progress... Stay tuned!").assertIsDisplayed()
+    }
+}
