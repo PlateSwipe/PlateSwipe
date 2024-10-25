41f7af23526290c130c00d5746f6004c1c80cfee
RagdollGoDown <zenooby2002@gmail.com>
2024-10-25 11:34:40 +0200

    change: corner radius and text typography of searchbar placeholder

--- app/src/main/java/com/android/sample/ui/utils/SearchBar.kt
+++ app/src/main/java/com/android/sample/ui/utils/SearchBar.kt
@@ -14,8 +14,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Preview
@@ -23,21 +25,34 @@ import androidx.compose.ui.unit.dp
 fun SearchBar(modifier: Modifier = Modifier, onValueChange: (String) -> Unit = {}) {
   var searchText by remember { mutableStateOf("") }
 
+  val cornerEdgeRadius: Dp = 16.dp
+
   TextField(
       value = searchText,
       onValueChange = {
         searchText = it
         onValueChange(it)
       },
-      modifier = modifier.testTag("searchBar"),
-      shape = RoundedCornerShape(16.dp),
+      modifier =
+          modifier
+              .testTag("searchBar")
+              .shadow(
+                  elevation = 8.dp,
+                  shape = RoundedCornerShape(cornerEdgeRadius),
+              ),
+      shape = RoundedCornerShape(cornerEdgeRadius),
       leadingIcon = {
         Icon(
             imageVector = Icons.Filled.Search,
             contentDescription = "search",
             tint = MaterialTheme.colorScheme.onPrimary)
       },
-      placeholder = { Text(text = "Search", color = MaterialTheme.colorScheme.onPrimary) },
+      placeholder = {
+        Text(
+            text = "Search",
+            color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.bodyMedium)
+      },
       colors =
           TextFieldDefaults.colors(
               focusedContainerColor = MaterialTheme.colorScheme.onSecondary,
