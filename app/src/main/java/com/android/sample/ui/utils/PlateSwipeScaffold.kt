2b02c0717451193edd5347eea3cdf72bbd919488
RagdollGoDown <zenooby2002@gmail.com>
2024-10-25 11:53:29 +0200

    change: ktfmt formatted file

--- app/src/main/java/com/android/sample/ui/utils/PlateSwipeScaffold.kt
+++ app/src/main/java/com/android/sample/ui/utils/PlateSwipeScaffold.kt
@@ -2,14 +2,12 @@ package com.android.sample.ui.utils
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -21,13 +19,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.compose.rememberNavController
 import com.android.sample.ui.navigation.BottomNavigationMenu
 import com.android.sample.ui.navigation.LIST_TOP_LEVEL_DESTINATIONS
 import com.android.sample.ui.navigation.NavigationActions
-import com.android.sample.ui.theme.SampleAppTheme
 
 /**
  * PlateSwipeScaffold is a custom Scaffold that is used in the PlateSwipe app. It has a custom top
