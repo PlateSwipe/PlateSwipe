d4120ba56808524d4f46d474793276afa3933d67
RagdollGoDown <zenooby2002@gmail.com>
2024-10-25 12:52:15 +0200

    remove: unused imports in AccountScreen

--- app/src/main/java/com/android/sample/ui/account/AccountScreen.kt
+++ app/src/main/java/com/android/sample/ui/account/AccountScreen.kt
@@ -1,8 +1,6 @@
 package com.android.sample.ui.account
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import com.android.sample.model.user.UserViewModel
 import com.android.sample.ui.EmptyScreen
 import com.android.sample.ui.navigation.NavigationActions
