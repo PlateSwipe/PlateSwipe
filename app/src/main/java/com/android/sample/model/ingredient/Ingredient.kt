d50bc0c4a13d123f13d92891993a54920c8a2725
Ylanv <ylan.vifian@epfl.ch>
2024-10-30 18:11:42 +0100

    feat(ing) : Add information about ingredient
    - Brands, item url image in 3 format

--- app/src/main/java/com/android/sample/model/ingredient/Ingredient.kt
+++ app/src/main/java/com/android/sample/model/ingredient/Ingredient.kt
@@ -1,3 +1,18 @@
 package com.android.sample.model.ingredient
 
-class Ingredient(val barCode: Long, val name: String) {}
+data class Ingredient(
+    val barCode: Long? = null,
+    val name: String? = null,
+    val selectedImages: SelectedImages? = null,
+    val brands: String? = null
+)
+
+data class SelectedImages(
+    val front: ImageUrls? = null
+)
+
+data class ImageUrls(
+    val display: String? = null,
+    val small: String? = null,
+    val thumb: String? = null
+)
