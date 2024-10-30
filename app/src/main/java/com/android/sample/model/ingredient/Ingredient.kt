0073081fb75176a9b2d2d457d16ee5286a2927b1
RagdollGoDown <zenooby2002@gmail.com>
2024-10-30 12:31:26 +0100

    revert: add quantity measurements
    shouldn't be done in this branch but in another, this only focuses on the implementation of the firestore database

--- app/src/main/java/com/android/sample/model/ingredient/Ingredient.kt
+++ app/src/main/java/com/android/sample/model/ingredient/Ingredient.kt
@@ -3,14 +3,4 @@ package com.android.sample.model.ingredient
 data class Ingredient(
     val barCode: Long,
     val name: String,
-    val quantity: Double,
-    val quantityMeasurement: QuantityMeasurement,
-    ) {}
-
-enum class QuantityMeasurement {
-    GRAM,
-    KILOGRAM,
-    LITER,
-    MILLILITER,
-    PIECE
-}
+)
