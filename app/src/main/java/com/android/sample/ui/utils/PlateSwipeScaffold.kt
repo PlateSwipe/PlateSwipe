b031850b1b76c0a4de69405383fbb0766d708aab
RagdollGoDown <zenooby2002@gmail.com>
2024-11-03 15:59:44 +0100

    remove: useless import

--- app/src/main/java/com/android/sample/ui/utils/PlateSwipeScaffold.kt
+++ app/src/main/java/com/android/sample/ui/utils/PlateSwipeScaffold.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
