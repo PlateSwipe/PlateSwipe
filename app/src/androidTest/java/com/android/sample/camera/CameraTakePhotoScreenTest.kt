55879697ce843c17d27d4851871b30a053f4da79
Ylanv <ylan.vifian@epfl.ch>
2024-11-05 10:24:40 +0100

    refactor : ktfmt format

--- app/src/androidTest/java/com/android/sample/camera/CameraTakePhotoScreenTest.kt
+++ app/src/androidTest/java/com/android/sample/camera/CameraTakePhotoScreenTest.kt
@@ -1,8 +1,6 @@
 package com.android.sample.camera
 
 import android.Manifest
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageProxy
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
