6a4f4c0b2087ea766d9c015f828ee80f7fed4e03
Erik Hübner <erik.hubner@proton.me>
2024-11-06 12:48:07 +0100

    chore: put constant string in strings.xml

--- app/src/main/java/com/android/sample/ui/createRecipe/RecipeInstructionsScreen.kt
+++ app/src/main/java/com/android/sample/ui/createRecipe/RecipeInstructionsScreen.kt
@@ -2,6 +2,8 @@ package com.android.sample.ui.createRecipe
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.android.sample.R
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Screen
 
@@ -12,9 +14,9 @@ fun RecipeInstructionsScreen(
     modifier: Modifier = Modifier
 ) {
   RecipeStepScreen(
-      title = "Add Instructions",
-      subtitle = "Describe each step of how to prepare your dish.",
-      buttonText = "Add Step",
+      title = stringResource(id = R.string.add_instructions),
+      subtitle = stringResource(id = R.string.describe_instructions),
+      buttonText = stringResource(id = R.string.add_step),
       onButtonClick = { navigationActions.navigateTo(Screen.CREATE_RECIPE_ADD_INSTRUCTION) },
       navigationActions = navigationActions,
       currentStep = currentStep,
