9d365b345d1e0c8f67a2d6c6878ed4ff67ef2e51
Ylanv <ylan.vifian@epfl.ch>
2024-11-07 14:46:16 +0100

    bugfix : take photo is slow
    - Add lauchnUnit to avoid recomp
    - Wait for rotation before displaying

--- app/src/main/java/com/android/sample/ui/camera/TestScreen.kt
+++ app/src/main/java/com/android/sample/ui/camera/TestScreen.kt
@@ -3,9 +3,7 @@ package com.android.sample.ui.camera
 import android.graphics.Bitmap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.testTag
@@ -23,11 +21,20 @@ fun DisplayImageScreen(takePhotoViewModel: TakePhotoViewModel) {
   val bitmap: Bitmap? by takePhotoViewModel.photo.collectAsState()
   // Collect the rotation value from the ViewModel's StateFlow
   val rotation: Int by takePhotoViewModel.rotation.collectAsState()
-  // Rotate the bitmap by the collected rotation value
-  val bitmapRotated = rotateBitmap(bitmap!!, rotation)
-  // Display the rotated bitmap as an image
-  Image(
-      bitmap = bitmapRotated.asImageBitmap(),
-      contentDescription = "Image taken from camera",
-      modifier = Modifier.fillMaxSize().testTag("display_image"))
+
+  var bitmapRotated by remember { mutableStateOf<Bitmap?>(null) }
+
+  // Use LaunchedEffect to ensure the image rotation logic is only executed once
+  LaunchedEffect(bitmap, rotation) {
+    if (bitmap != null) {
+      bitmapRotated = rotateBitmap(bitmap!!, rotation)
+    }
+  }
+
+  bitmapRotated?.let {
+    Image(
+        bitmap = it.asImageBitmap(),
+        contentDescription = "Image taken from camera",
+        modifier = Modifier.fillMaxSize().testTag("display_image"))
+  }
 }
