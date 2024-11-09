391ee6eb498ca44745acaf8ec2069144067988d0
Erik Hübner <erik.hubner@proton.me>
2024-11-09 17:00:54 +0100

    feat: implement RecipeListInstructionsScreen

---
+++ app/src/main/java/com/android/sample/ui/createRecipe/RecipeListInstructionsScreen.kt
@@ -0,0 +1,183 @@
+package com.android.sample.ui.createRecipe
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ModeEdit
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.android.sample.R
+import com.android.sample.model.recipe.CreateRecipeViewModel
+import com.android.sample.model.recipe.RecipeBuilder
+import com.android.sample.resources.C.Dimension.CameraScanCodeBarScreen.BOTTOM_BAR_HEIGHT
+import com.android.sample.resources.C.Dimension.CameraScanCodeBarScreen.TOP_BAR_HEIGHT
+import com.android.sample.resources.C.Tag.RECIPE_NAME_BASE_PADDING
+import com.android.sample.resources.C.Tag.RECIPE_NAME_BUTTON_HEIGHT
+import com.android.sample.resources.C.Tag.RECIPE_NAME_BUTTON_WIDTH
+import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.navigation.Route
+import com.android.sample.ui.navigation.Screen
+import com.android.sample.ui.theme.lightCream
+import com.android.sample.ui.utils.PlateSwipeScaffold
+
+@Composable
+fun RecipeListInstructionsScreen(
+    createRecipeViewModel: CreateRecipeViewModel,
+    navigationActions: NavigationActions
+) {
+  /** **************************Testing stuff*************************** */
+  val navController = rememberNavController()
+  val recipeBuilder = RecipeBuilder()
+  recipeBuilder.setName("Recipe name")
+  recipeBuilder.setInstructions("Instruction1")
+  recipeBuilder.setTime("10")
+  /** ****************************************************************** */
+  PlateSwipeScaffold(
+      navigationActions = NavigationActions(navController = navController),
+      selectedItem = Route.CREATE_RECIPE,
+      showBackArrow = true,
+      content = { pd ->
+        RecipeListInstructionsContent(
+            pd = pd,
+            recipeBuilder = recipeBuilder,
+            modifier = Modifier,
+            navigationActions = navigationActions)
+      })
+}
+
+@Composable
+fun RecipeListInstructionsContent(
+    pd: PaddingValues,
+    recipeBuilder: RecipeBuilder,
+    modifier: Modifier,
+    navigationActions: NavigationActions
+) {
+
+  Column(
+      modifier =
+          modifier
+              .padding(top = TOP_BAR_HEIGHT.dp, bottom = BOTTOM_BAR_HEIGHT.dp)
+              .fillMaxSize()
+              .padding(RECIPE_NAME_BASE_PADDING)) {
+        RecipeProgressBar(currentStep = 2)
+        Spacer(modifier = Modifier.height(32.dp).testTag("RecipeListInstructionsScreenSpacer1"))
+        Text(
+            modifier = Modifier.testTag("RecipeNameText"),
+            text = recipeBuilder.getName(),
+            style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+        )
+        Text(
+            modifier = Modifier.testTag("InstructionsText"),
+            text = "Instructions",
+            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+        )
+        // LazyColumn for the scrollable list of instructions
+        LazyColumn(
+            contentPadding = PaddingValues(vertical = 8.dp),
+            modifier =
+                modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.8899f)
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 8.dp, bottom = 8.dp)
+                    .testTag("InstructionList")) {
+              items(5) { index -> InstructionValue(index, recipeBuilder.getTime(), 0, {}) }
+            }
+        Spacer(modifier = Modifier.weight(1f).testTag("RecipeListInstructionsScreenSpacer2"))
+        // Fixed button at the bottom
+        NextStepButton(
+            modifier = modifier.align(Alignment.CenterHorizontally),
+            navigationActions = navigationActions)
+      }
+}
+
+@Composable
+fun NextStepButton(modifier: Modifier, navigationActions: NavigationActions) {
+  Button(
+      onClick = { navigationActions.navigateTo(Screen.PUBLISH_CREATED_RECIPE) },
+      modifier =
+          modifier
+              .width(RECIPE_NAME_BUTTON_WIDTH)
+              .height(RECIPE_NAME_BUTTON_HEIGHT)
+              .background(color = lightCream, shape = RoundedCornerShape(size = 4.dp))
+              .testTag("NextStepButton"),
+      colors = ButtonDefaults.buttonColors(lightCream, contentColor = Color.Black),
+      shape = RoundedCornerShape(4.dp)) {
+        Text(stringResource(R.string.next_step))
+      }
+}
+
+@Composable
+fun InstructionValue(index: Int, time: String?, icon: Int, onClick: () -> Unit) {
+  val officialStep = index + 1
+  Card(
+      modifier =
+          Modifier.testTag("InstructionListItem")
+              .fillMaxWidth()
+              .padding(vertical = 4.dp)
+              .clickable(onClick = onClick)
+              .border(1.dp, Color.Gray, RoundedCornerShape(8.dp)),
+      colors =
+          CardDefaults.cardColors(
+              containerColor = Color.White, // Background color of the card
+              contentColor = Color.Black // Content color of the card
+              ),
+  ) {
+    Column(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+      // Date and Status Row
+      Row(modifier = Modifier.fillMaxWidth(1f).testTag("InstructionThumbnail"), horizontalArrangement = Arrangement.SpaceBetween) {
+        Image(
+            painter = painterResource(id = R.drawable.fire),
+            contentDescription = "Fire",
+            modifier = Modifier.size(24.dp).testTag("InstructionIcon"))
+        Column(modifier = Modifier.testTag("InstructionTextSpace")) {
+          Text(
+              modifier = Modifier.testTag("InstructionText"),
+              text = "Step $officialStep",
+              style = MaterialTheme.typography.bodyMedium,
+              fontWeight = FontWeight.Bold)
+
+
+          if (!time.isNullOrBlank()) {
+            Text(text = "$time min", style = MaterialTheme.typography.bodySmall, modifier = Modifier.testTag("InstructionTime"))
+          }
+        }
+        Icon(
+            imageVector = Icons.Default.ModeEdit,
+            contentDescription = "Edit",
+            modifier = Modifier.size(24.dp).testTag("EditInstructionIcon"))
+      }
+    }
+  }
+}
