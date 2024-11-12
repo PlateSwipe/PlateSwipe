edff692aafc63094158038d51b8492500efa0bdf
Anas El madhi <anas.elmadhi@epfl.ch>
2024-11-12 12:16:08 +0100

    add changes to searchBar composable

--- app/src/main/java/com/android/sample/ui/utils/SearchBar.kt
+++ app/src/main/java/com/android/sample/ui/utils/SearchBar.kt
@@ -1,34 +1,27 @@
 package com.android.sample.ui.utils
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.android.sample.R
-import com.android.sample.model.recipe.Recipe
-import com.android.sample.resources.C.Tag.SEARCH_BAR_PLACE_HOLDER
+import com.android.sample.resources.C.Tag.SEARCH_BAR_CORNER_RADIUS
 
 /**
  * A search bar that allows users to search for recipes.
@@ -38,53 +31,43 @@ import com.android.sample.resources.C.Tag.SEARCH_BAR_PLACE_HOLDER
  */
 @Preview
 @Composable
-fun SearchBar(
-    modifier: Modifier = Modifier,
-    list: List<Recipe> = emptyList(),
-    onValueChange: (String) -> Unit = {}
-) {
-  var searchText by remember { mutableStateOf("") }
-  var isFocused by remember { mutableStateOf(false) }
-  Row(
-      horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.Start),
-      verticalAlignment = Alignment.CenterVertically,
-  ) {
-    Image(
-        painter = painterResource(id = R.drawable.search),
-        contentDescription = "search icon",
-        modifier =
-            Modifier.padding(0.dp).width(20.2643.dp).height(20.72197.dp).testTag("search icon"))
+fun SearchBar(modifier: Modifier = Modifier, onValueChange: (String) -> Unit = {}) {
+    var searchText by remember { mutableStateOf("") }
+
     TextField(
-        value = if (isFocused) "$searchText|" else searchText,
+        value = searchText,
         onValueChange = {
-          if (isFocused) searchText = it.dropLast(1) else searchText = it
-          filter(searchText, list)
-          onValueChange(searchText)
+            searchText = it
+            onValueChange(it)
         },
-        textStyle =
-            TextStyle(
-                fontSize = 16.sp,
-                lineHeight = 20.sp,
-                fontFamily = FontFamily(Font(R.font.montserrat_light)),
-                fontWeight = FontWeight(400),
-                color = Color(0xFF131313),
-            ),
-        placeholder = { Text(SEARCH_BAR_PLACE_HOLDER, fontSize = 16.sp) },
         modifier =
-            Modifier.width(253.dp)
-                .height(40.dp)
-                .onFocusChanged { focusState -> isFocused = focusState.isFocused }
-                .testTag("searchBar"))
-  }
+        modifier
+            .testTag("searchBar")
+            .shadow(
+                elevation = 8.dp,
+                shape = RoundedCornerShape(SEARCH_BAR_CORNER_RADIUS.dp),
+            ),
+        shape = RoundedCornerShape(SEARCH_BAR_CORNER_RADIUS.dp),
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = "searchIcon",
+                tint = MaterialTheme.colorScheme.onPrimary)
+        },
+        placeholder = {
+            Text(
+                text = stringResource(R.string.search_bar_place_holder),
+                color = MaterialTheme.colorScheme.onPrimary,
+                style = MaterialTheme.typography.bodyMedium)
+        },
+        colors =
+        TextFieldDefaults.colors(
+            focusedContainerColor = MaterialTheme.colorScheme.onSecondary,
+
+            // we need to make these transparent or a weird line appears
+            focusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ))
 }
 
-/**
- * Filters the list of recipes based on the search string.
- *
- * @param string the search string to filter the list of recipes.
- * @param list the list of recipes to filter.
- * @return the filtered list of recipes.
- */
-fun filter(string: String, list: List<Recipe>): List<Recipe> {
-  return list.filter { it.strMeal.contains(string) }
-}
