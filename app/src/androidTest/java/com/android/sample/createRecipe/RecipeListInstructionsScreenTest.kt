7c9883182998f12212b3ae10d2937af882e0dc20
Erik Hübner <erik.hubner@proton.me>
2024-11-12 22:58:49 +0100

    test: add tests to see if everything is clickable

--- app/src/androidTest/java/com/android/sample/createRecipe/RecipeListInstructionsScreenTest.kt
+++ app/src/androidTest/java/com/android/sample/createRecipe/RecipeListInstructionsScreenTest.kt
@@ -1,11 +1,13 @@
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.test.espresso.intent.Intents
+import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.sample.model.recipe.CreateRecipeViewModel
 import com.android.sample.model.recipe.FirestoreRecipesRepository
+import com.android.sample.resources.C.TestTag.CreateRecipeListInstructionsScreen.INSTRUCTION_LIST_ITEM
 import com.android.sample.resources.C.TestTag.CreateRecipeListInstructionsScreen.INSTRUCTION_TEXT
+import com.android.sample.resources.C.TestTag.CreateRecipeListInstructionsScreen.NEXT_STEP_BUTTON
 import com.android.sample.resources.C.TestTag.CreateRecipeListInstructionsScreen.RECIPE_NAME_TEXT
 import com.android.sample.resources.C.TestTag.CreateRecipeListInstructionsScreen.SCREEN_COLUMN
 import com.android.sample.ui.createRecipe.RecipeListInstructionsScreen
@@ -13,7 +15,6 @@ import com.android.sample.ui.navigation.NavigationActions
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
-import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -33,28 +34,27 @@ class RecipeListInstructionsScreenTest {
     navigationActions = mockk(relaxed = true)
     createRecipeViewModel = spyk(CreateRecipeViewModel(repository))
 
-    Intents.init()
-
     every { createRecipeViewModel.getRecipeName() } returns "Recipe Name"
     every { createRecipeViewModel.getRecipeTime() } returns "30"
     every { createRecipeViewModel.getRecipeInstructions() } returns "Instructions"
-  }
 
-  @After
-  fun tearDown() {
-    Intents.release()
+    composeTestRule.setContent {
+      RecipeListInstructionsScreen(
+          navigationActions = navigationActions, createRecipeViewModel = createRecipeViewModel)
+    }
   }
 
   /** Verifies that all UI elements are displayed on the RecipeListInstructionsScreen. */
   @Test
   fun recipeListInstructionsScreen_allFieldsDisplayed() {
-    composeTestRule.setContent {
-      RecipeListInstructionsScreen(
-          navigationActions = navigationActions, createRecipeViewModel = createRecipeViewModel)
-    }
-
     composeTestRule.onNodeWithTag(SCREEN_COLUMN).assertIsDisplayed()
     composeTestRule.onNodeWithTag(RECIPE_NAME_TEXT).assertIsDisplayed()
     composeTestRule.onNodeWithTag(INSTRUCTION_TEXT).assertIsDisplayed()
+  }
+
+  @Test
+  fun recipeListInstructionsScreenAllClickableFieldsTest() {
+    composeTestRule.onNodeWithTag(NEXT_STEP_BUTTON).performClick()
+    composeTestRule.onNodeWithTag(INSTRUCTION_LIST_ITEM).performClick()
   }
 }
