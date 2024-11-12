0d0c8237e82234bd41a3091f1c56639d6ae97ee6
Anas El madhi <anas.elmadhi@epfl.ch>
2024-11-12 10:24:53 +0100

    pass display tests

--- app/src/androidTest/java/com/android/sample/recipe/SearchRecipeScreenTest.kt
+++ app/src/androidTest/java/com/android/sample/recipe/SearchRecipeScreenTest.kt
@@ -1,21 +1,16 @@
 package com.android.sample.utils
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
-import androidx.test.espresso.intent.Intents
 import com.android.sample.model.recipe.Recipe
 import com.android.sample.model.recipe.RecipesRepository
 import com.android.sample.model.recipe.RecipesViewModel
+import com.android.sample.resources.C.Tag.SEARCH_BAR_PLACE_HOLDER
 import com.android.sample.ui.navigation.NavigationActions
 import com.android.sample.ui.navigation.Screen
-import com.android.sample.ui.utils.RecipeList
-import com.android.sample.ui.utils.TopCornerLikeButton
-import org.junit.After
+import com.android.sample.ui.recipeOverview.SearchRecipeScreen
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -26,11 +21,22 @@ import org.mockito.kotlin.anyOrNull
 
 class searchRecipeScreenTest {
   private lateinit var mockNavigationActions: NavigationActions
-  private lateinit var repository: RecipesRepository
+  private lateinit var mockRepository: RecipesRepository
   private lateinit var recipesViewModel: RecipesViewModel
+  private lateinit var recipesList: List<Recipe>
 
-  private val recipesList: List<Recipe> =
-      listOf(
+  @get:Rule val composeTestRule = createComposeRule()
+
+  @Before
+  fun setUp() {
+    mockNavigationActions = mock(NavigationActions::class.java)
+    mockRepository = mock(RecipesRepository::class.java)
+    recipesViewModel = RecipesViewModel(mockRepository)
+
+    `when`(mockRepository.random(eq(1), anyOrNull(), anyOrNull())).then {}
+    `when`(mockNavigationActions.currentRoute()).thenReturn(Screen.SEARCH)
+
+    recipesList = listOf(
           Recipe(
               "0",
               "Meal1",
@@ -76,74 +82,19 @@ class searchRecipeScreenTest {
               "https://img.jakpost.net/c/2016/09/29/2016_09_29_12990_1475116504._large.jpg",
               listOf(Pair("1", "peu"), Pair("2", "beaucoup"), Pair("3", "peu")),
           ))
-
-  private val testRecipe =
-      Recipe(
-          idMeal = "12345",
-          strMeal = "Test Recipe",
-          strCategory = "Test Category",
-          strArea = "Test Area",
-          strInstructions = "Test Instructions",
-          strMealThumbUrl = "https://example.com/image.jpg",
-          ingredientsAndMeasurements = listOf(Pair("1", "Test Ingredient")),
-      )
-
-  @get:Rule val composeTestRule = createComposeRule()
-
-  @Before
-  fun setUp() {
-    mockNavigationActions = mock(NavigationActions::class.java)
-    repository = mock(RecipesRepository::class.java)
-    recipesViewModel = RecipesViewModel(repository)
-    `when`(repository.random(eq(1), anyOrNull(), anyOrNull())).then {}
-    Intents.init()
-    `when`(mockNavigationActions.currentRoute()).thenReturn(Screen.SEARCH)
-  }
-
-  @After
-  fun tearDown() {
-    Intents.release()
   }
 
   @Test
-  fun testCorrectlyElements() {
-    composeTestRule.setContent {
-      RecipeList(
-          modifier = Modifier.fillMaxSize(),
-          list = recipesList,
-          onRecipeSelected = {},
-          topCornerButton = { recipe -> TopCornerLikeButton(recipe = recipe) })
-    }
+  fun displayTest() {
+    composeTestRule.setContent { SearchRecipeScreen(mockNavigationActions, recipesViewModel) }
+      composeTestRule.onNodeWithTag("SearchRecipeScreen").assertIsDisplayed()
+      composeTestRule.onNodeWithTag("PlateSwipeLogo icon").assertIsDisplayed()
+      composeTestRule.onNodeWithTag("PlateSwipeLogo text").assertIsDisplayed()
+      composeTestRule.onNodeWithTag("filter button").assertIsDisplayed()
+      composeTestRule.onNodeWithTag("search icon").assertIsDisplayed()
+      composeTestRule.onNodeWithTag("searchBar").assertIsDisplayed()
 
-    composeTestRule
-        .onAllNodesWithTag("recipeCard", useUnmergedTree = true)
-        .assertCountEquals(recipesList.count())
-    composeTestRule
-        .onAllNodesWithTag("recipeTitle", useUnmergedTree = true)
-        .assertCountEquals(recipesList.count())
-    composeTestRule
-        .onAllNodesWithTag("recipeImage", useUnmergedTree = true)
-        .assertCountEquals(recipesList.count())
-    composeTestRule
-        .onAllNodesWithTag("recipeFavoriteIcon", useUnmergedTree = true)
-        .assertCountEquals(recipesList.count())
-  }
-
-  @Test
-  fun testOnRecipeSelectedIsCalledOnSelection() {
-    var selected = false
-
-    val onRecipeSelected: (Recipe) -> Unit = { recipe ->
-      assert(recipe == testRecipe)
-      selected = true
-    }
-
-    composeTestRule.setContent {
-      RecipeList(listOf(testRecipe), onRecipeSelected = onRecipeSelected)
-    }
-
-    composeTestRule.onNodeWithTag("recipeCard", useUnmergedTree = true).performClick()
-    composeTestRule.waitForIdle()
-    assert(selected)
+      composeTestRule.onNodeWithTag("searchBar")
+          .assertTextEquals(SEARCH_BAR_PLACE_HOLDER)
   }
 }
