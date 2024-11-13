5d946a00be0e5d8d4ba76c53e9b24e51be2a4a93
Anas El madhi <anas.elmadhi@epfl.ch>
2024-11-13 19:13:49 +0100

    add filer to searchScreen

--- app/src/main/java/com/android/sample/ui/recipe/SearchRecipeScreen.kt
+++ app/src/main/java/com/android/sample/ui/recipe/SearchRecipeScreen.kt
@@ -1,19 +1,23 @@
-package com.android.sample.ui.recipeOverview
+package com.android.sample.ui.recipe
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.android.sample.R
 import com.android.sample.model.recipe.Recipe
+import com.android.sample.resources.C.Tag.PADDING
+import com.android.sample.resources.C.Tag.SMALL_PADDING
 import com.android.sample.resources.C.TestTag.SearchScreen.SEARCH_SCREEN
 import com.android.sample.ui.navigation.NavigationActions
+import com.android.sample.ui.navigation.Screen
 import com.android.sample.ui.utils.*
-import com.android.sample.ui.theme.*
 
 @Composable
 fun SearchRecipeScreen(navigationActions: NavigationActions, recipelist: List<Recipe>) {
@@ -28,26 +32,34 @@ fun SearchScreenContent(paddingValues: PaddingValues, recipeList: List<Recipe>, 
     Column(
         modifier = Modifier.fillMaxSize().padding(paddingValues).padding(16.dp).testTag(SEARCH_SCREEN) ,
         verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.Top),
-        horizontalAlignment = Alignment.CenterHorizontally,) {
-            SearchBar(modifier =
-            Modifier.shadow(
-                elevation = 4.dp, spotColor = C4, ambientColor = C4)
-                .border(width = 1.dp, color = C4)
-                .width(329.dp)
-                .height(64.dp)
-                .background(color = lightCream)
-                .padding(start = 16.dp, top = 8.dp, end = 8.dp, bottom = 16.dp).testTag("searchBar"))
+        horizontalAlignment = Alignment.CenterHorizontally) {
 
-            RecipeList(
-                list = recipeList,
-                onRecipeSelected = { _ ->
-                    navigationActions.navigateTo("Overview Recipe Screen")
-                },
-                topCornerButton = { recipe -> TopCornerLikeButton(recipe)
-                },
-                modifier = Modifier.fillMaxSize().padding(top = 16.dp, bottom = 16.dp).testTag("SearchRecipeList")
-            )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            SearchBar(modifier = Modifier.weight(1f), isSingleLine = true)
+            Spacer(modifier = Modifier.size(SMALL_PADDING.dp))
+            Icon(
+                painter = painterResource(id = R.drawable.filter),
+                contentDescription = null,
+                modifier = Modifier.testTag("filter icon").weight(0.1f).clickable {
+                    navigationActions.navigateTo(Screen.FILTER)
+                })
+
         }
+
+        Spacer(modifier = Modifier.size(PADDING.dp))
+
+        RecipeList(
+            list = recipeList,
+            onRecipeSelected = { _ ->
+                navigationActions.navigateTo("Overview Recipe Screen")
+            },
+            topCornerButton = { recipe -> TopCornerLikeButton(recipe)
+            },
+            modifier = Modifier.fillMaxSize().padding(top = 16.dp, bottom = 16.dp).testTag("SearchRecipeList")
+        )
+    }
 }
-
-
