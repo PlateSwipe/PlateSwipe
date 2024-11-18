58edfa40b17cde994f8cb9f351a9ebb718e4dbde
Erik Hübner <erik.hubner@proton.me>
2024-11-18 11:23:11 +0100

    doc: update documentation

--- app/src/main/java/com/android/sample/model/recipe/Instruction.kt
+++ app/src/main/java/com/android/sample/model/recipe/Instruction.kt
@@ -7,7 +7,8 @@ import com.android.sample.resources.C.TestTag.Instruction.DEFAULT_ICON
  *
  * @property description The description of the instruction.
  * @property time The time required to complete the instruction in minutes. Nullable.
- * @property icon The icon representing the instruction.
+ * @property icon The icon representing the instruction, see IconType class to see the different
+ *   icons.
  */
 data class Instruction(
     val description: String,
@@ -17,6 +18,7 @@ data class Instruction(
   private val icon: IconType =
       if (!iconType.isNullOrEmpty()) IconType(iconType) else IconType(DEFAULT_ICON)
 
+  /** Returns the icon representing the instruction. */
   fun icon(): IconType {
     return icon
   }
