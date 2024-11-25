f591d5a7a8f59de800edaef1b325b677eda542cb
2Fick <youcef.amar@epfl.ch>
2024-11-25 18:37:03 +0100

    fix: add missing import in RecipeOverviewTest.kt

--- app/src/androidTest/java/com/android/sample/screen/RecipeOverviewTest.kt
+++ app/src/androidTest/java/com/android/sample/screen/RecipeOverviewTest.kt
@@ -23,6 +23,7 @@ import com.android.sample.resources.C.TestTag.RecipeOverview.COOK_TIME_TEXT
 import com.android.sample.resources.C.TestTag.RecipeOverview.DRAGGABLE_ITEM
 import com.android.sample.resources.C.TestTag.RecipeOverview.INGREDIENTS_VIEW
 import com.android.sample.resources.C.TestTag.RecipeOverview.INGREDIENT_CHECKBOX
+import com.android.sample.resources.C.TestTag.RecipeOverview.INSTRUCTIONS_VIEW
 import com.android.sample.resources.C.TestTag.RecipeOverview.NUMBER_SERVINGS
 import com.android.sample.resources.C.TestTag.RecipeOverview.PREP_TIME_TEXT
 import com.android.sample.resources.C.TestTag.RecipeOverview.RECIPE_IMAGE
