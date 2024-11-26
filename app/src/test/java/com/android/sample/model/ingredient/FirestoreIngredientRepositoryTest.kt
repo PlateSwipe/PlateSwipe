33cb0a7f01243a2ea5b03fb2c4bf2d3aae02bccb
RagdollGoDown <zenooby2002@gmail.com>
2024-11-26 22:33:02 +0100

    change: made it impossible to add ingredients with the same barcode in FirestoreIngredientRepository Now when an element exists in the firestore db with the same barcode it will either update it, if the updateIfExists param is true, or add it
    There is also a new excepetion for when the ingredient has no barcode and someone is attempting to add it to the db.

--- app/src/test/java/com/android/sample/model/ingredient/FirestoreIngredientRepositoryTest.kt
+++ app/src/test/java/com/android/sample/model/ingredient/FirestoreIngredientRepositoryTest.kt
@@ -177,7 +177,12 @@ class FirestoreIngredientRepositoryTest {
   fun testAddGivesIdWhenAddingIngredientThatDoesntHaveOne() {
     val nullUidIngredient = ingredient.copy(uid = null)
 
+    // we need an empty list to simulate an unfound ingredient
+    `when`(mockQuerySnapshot.documents).thenReturn(listOf())
+
     firestoreIngredientRepository.add(nullUidIngredient, onSuccess = {}, onFailure = {})
+
+    onCompleteListenerQueryCaptor.value.onComplete(mockTaskQuerySnapshot)
 
     verify(mockDocumentReference).set(capture(objectCapture))
 
@@ -189,7 +194,10 @@ class FirestoreIngredientRepositoryTest {
 
   @Test
   fun testAddCorrectlyAddsIngredient() {
-    firestoreIngredientRepository.add(ingredient, onSuccess = {}, onFailure = {})
+    firestoreIngredientRepository.add(
+        ingredient, onSuccess = {}, onFailure = {}, updateIfPresent = true)
+
+    onCompleteListenerQueryCaptor.value.onComplete(mockTaskQuerySnapshot)
 
     verify(mockDocumentReference).set(capture(objectCapture))
 
@@ -199,14 +207,39 @@ class FirestoreIngredientRepositoryTest {
   }
 
   @Test
-  fun testAddThrowsExceptionWhenUnsuccessful() {
+  fun testAddThrowsExceptionWhenUnsuccessfulAdd() {
+    `when`(mockTaskVoid.isSuccessful).thenReturn(false)
+    `when`(mockTaskVoid.exception).thenReturn(Exception("Error"))
+
+    // we need an empty list to simulate an unfound ingredient
+    `when`(mockQuerySnapshot.documents).thenReturn(listOf())
+
+    var resultingException: Exception? = null
+
+    firestoreIngredientRepository.add(
+        ingredient, onSuccess = {}, onFailure = { e -> resultingException = e })
+
+    onCompleteListenerQueryCaptor.value.onComplete(mockTaskQuerySnapshot)
+
+    onCompleteListenerVoidCaptor.value.onComplete(mockTaskVoid)
+
+    assertNotNull(resultingException)
+  }
+
+  @Test
+  fun testAddThrowsExceptionWhenUnsuccessfulUpdate() {
     `when`(mockTaskVoid.isSuccessful).thenReturn(false)
     `when`(mockTaskVoid.exception).thenReturn(Exception("Error"))
 
     var resultingException: Exception? = null
 
     firestoreIngredientRepository.add(
-        ingredient, onSuccess = {}, onFailure = { e -> resultingException = e })
+        ingredient,
+        onSuccess = {},
+        onFailure = { e -> resultingException = e },
+        updateIfPresent = true)
+
+    onCompleteListenerQueryCaptor.value.onComplete(mockTaskQuerySnapshot)
 
     onCompleteListenerVoidCaptor.value.onComplete(mockTaskVoid)
 
