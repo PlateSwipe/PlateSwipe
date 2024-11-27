be0a7b84d125f409e88d422f9d4056536e4b8d73
Adrien Bader <adrien.bader05@gmail.com>
2024-11-27 23:15:02 +0100

    test: add tests for ConfirmationPopUp util

--- app/src/androidTest/java/com/android/sample/utils/ConfirmationPopUpTest.kt
+++ app/src/androidTest/java/com/android/sample/utils/ConfirmationPopUpTest.kt
@@ -1,10 +1,12 @@
 package com.android.sample.utils
 
+import androidx.compose.ui.semantics.SemanticsActions
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performSemanticsAction
 import com.android.sample.resources.C.TestTag.IngredientSearchScreen.CANCEL_BUTTON
 import com.android.sample.resources.C.TestTag.IngredientSearchScreen.CONFIRMATION_BUTTON
 import com.android.sample.resources.C.TestTag.IngredientSearchScreen.CONFIRMATION_POPUP
@@ -35,7 +37,6 @@ class ConfirmationPopUpTest {
 
   @Test
   fun testConfirmButton_onClick_triggersOnConfirm() {
-
     // Act
     composeTestRule.onNodeWithTag(CONFIRMATION_BUTTON).performClick()
 
@@ -69,5 +70,16 @@ class ConfirmationPopUpTest {
   fun testPopUpIsVisible() {
     // Assert
     composeTestRule.onNodeWithTag(CONFIRMATION_POPUP).assertIsDisplayed() // Pop-up is visible
+  }
+
+  @Test
+  fun testOnDismissRequest_triggersOnDismiss() {
+    // Act
+    composeTestRule
+        .onNodeWithTag(CONFIRMATION_POPUP)
+        .performSemanticsAction(SemanticsActions.Dismiss)
+
+    // Assert
+    verify(onDismiss).invoke() // Check if onDismiss callback was triggered
   }
 }
