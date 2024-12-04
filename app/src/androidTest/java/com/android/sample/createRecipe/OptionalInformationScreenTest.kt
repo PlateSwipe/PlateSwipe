a75f549cb2d411ff2e46a2ebed87d00780446b54
RagdollGoDown <zenooby2002@gmail.com>
2024-12-04 10:39:08 +0100

    add: difficulty dropdown test in OptionalInformationScreenTest

--- app/src/androidTest/java/com/android/sample/createRecipe/OptionalInformationScreenTest.kt
+++ app/src/androidTest/java/com/android/sample/createRecipe/OptionalInformationScreenTest.kt
@@ -13,8 +13,10 @@ import androidx.compose.ui.test.performScrollTo
 import com.android.sample.model.image.ImageRepositoryFirebase
 import com.android.sample.model.recipe.CreateRecipeViewModel
 import com.android.sample.model.recipe.FirestoreRecipesRepository
+import com.android.sample.model.recipe.Recipe
 import com.android.sample.resources.C.TestTag.Category.BUTTON_TEST_TAG
 import com.android.sample.resources.C.TestTag.Category.CATEGORY_DROPDOWN
+import com.android.sample.resources.C.TestTag.Category.DIFFICULTY_DROPDOWN
 import com.android.sample.resources.C.TestTag.PlateSwipeDropdown.DROPDOWN_TITLE
 import com.android.sample.ui.createRecipe.OptionalInformationScreen
 import com.android.sample.ui.navigation.NavigationActions
@@ -92,6 +94,35 @@ class OptionalInformationScreenTest {
         .onAllNodesWithTag(DROPDOWN_TITLE, useUnmergedTree = true)
         .assertCountEquals(2)
         .assertAny(hasText(selectedCategory))
+  }
+
+  @Test
+  fun testSelectingDifficultyUpdatesButtonText() {
+    composeTestRule.setContent {
+      OptionalInformationScreen(
+        navigationActions = mockNavigationActions, createRecipeViewModel = createRecipeViewModel)
+    }
+
+    val selectedDifficulty = Recipe.getDifficulties()[1]
+
+    // Open dropdown menu
+    composeTestRule.onNodeWithTag(DIFFICULTY_DROPDOWN).performClick()
+
+    composeTestRule.onNodeWithText(selectedDifficulty, useUnmergedTree = true).performScrollTo()
+
+    // Wait for the UI to update
+    composeTestRule.waitForIdle()
+
+    // Select the category
+    composeTestRule.onNodeWithText(selectedDifficulty).performClick()
+
+    composeTestRule.waitForIdle()
+
+    // Verify dropdown button text updates to the selected category
+    composeTestRule
+      .onAllNodesWithTag(DROPDOWN_TITLE, useUnmergedTree = true)
+      .assertCountEquals(2)
+      .assertAny(hasText(selectedDifficulty))
   }
 
   @Test
